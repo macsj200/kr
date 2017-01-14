85bbffd10e1478e1fcf33593b462f1a7a54fa5a7
macsj200 <maxjohansen@berkeley.edu>
2017-01-14 13:56:28 -0800

    add a few exercises

---
+++ tempconvert.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  /* convert Celsius to Fahrenheit */
+  float celsius, fahrenheit;
+  float min_celsius, max_celsius, celsius_step;
+
+  // initialize temperature constants
+  min_celsius = -100.0;
+  max_celsius = 100.0;
+  celsius_step = 10.0;
+
+  // initialize temperature value
+  celsius = min_celsius;
+
+  // print header
+  printf("Celsius to Fahrenheit\n");
+
+  // print table
+  while(celsius <= max_celsius) {
+    fahrenheit = (9.0/5.0 * celsius) + 32;
+    printf("%6.2f %6.2f\n", celsius, fahrenheit);
+
+    celsius += celsius_step;
+  }
+
+  return 0;
+}
